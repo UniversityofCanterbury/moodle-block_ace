174df4f7491079d5863dc3163730d24fd8899e6b
Dan Marsden <dan@danmarsden.com>
2021-11-03 15:00:51 +1300

    bump version.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_ace';
 $plugin->release = '1.0';
-$plugin->version = 2021102700;
+$plugin->version = 2021110300;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
